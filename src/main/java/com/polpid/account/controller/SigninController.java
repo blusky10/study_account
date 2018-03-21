e51131d0e6d3992c223bd9d38ec4138a65b28e27
김상현 <sanghyun0910.kim@samsung.com>
2018-03-21 17:21:40 +0900

    siginin 추가

--- src/main/java/com/polpid/account/controller/SigninController.java
+++ src/main/java/com/polpid/account/controller/SigninController.java
@@ -2,18 +2,29 @@ package com.polpid.account.controller;
 
 import com.polpid.account.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 
 @Controller
 public class SigninController {
 
     @RequestMapping(value="/signin", method = RequestMethod.GET)
-    public ModelAndView showRegistrationPage(ModelAndView modelAndView, User user){
+    public ModelAndView showSigninPage(ModelAndView modelAndView, User user){
         modelAndView.addObject("user", user);
         modelAndView.setViewName("signin");
+        return modelAndView;
+    }
+
+    @RequestMapping(value="/signin", method = RequestMethod.POST)
+    public ModelAndView sigin(ModelAndView modelAndView, @Valid User user, BindingResult bindingResult, HttpServletRequest request){
+        modelAndView.setViewName("signin");
+
         return modelAndView;
     }
 }
