d5543c2f8c1e4c7c5b53fcc051faffa5de69beae
김상현 <sanghyun0910.kim@samsung.com>
2018-04-10 18:45:02 +0900

    @Repository 어노테이션 제거

--- src/main/java/com/polpid/account/repository/UserRepository.java
+++ src/main/java/com/polpid/account/repository/UserRepository.java
@@ -2,11 +2,10 @@ package com.polpid.account.repository;
 
 import com.polpid.account.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+
 public interface UserRepository extends JpaRepository<User, Long>{
     Optional<User> findByEmail(String email);
     Optional<User> findByConfirmationToken(String confirmationToken);
