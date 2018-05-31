cd49bddfd1470f3013ac93bd424c53793c442883
김상현 <sanghyun0910.kim@samsung.com>
2018-05-31 13:41:24 +0900

    Update: Security config

--- src/main/java/com/polpid/account/config/SecurityConfig.java
+++ src/main/java/com/polpid/account/config/SecurityConfig.java
@@ -1,16 +1,26 @@
 package com.polpid.account.config;
 
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @EnableWebSecurity
+@EnableOAuth2Client
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/register", "/confirm").permitAll();
+                .antMatchers("/register", "/confirm", "/home").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+                .antMatchers("/webjars/**","/resources/**", "/css/**", "/favicon.ico");
     }
 }
